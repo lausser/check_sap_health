c5e0b9cfd58f162d9ed744e27ae692ac71f68af0
Gerhard Lausser <gerhard.lausser@consol.de>
2014-04-22 23:53:25 +0200

    redesign

--- plugins-scripts/check_sap_health.pl
+++ plugins-scripts/check_sap_health.pl
@@ -1,63 +1,64 @@
-#! /usr/bin/perl
+# /usr/bin/perl -w
 
 use strict;
-use Digest::MD5 qw(md5_hex);;
+use File::Basename;
 
-use vars qw ($PROGNAME $REVISION $CONTACT $TIMEOUT $STATEFILESDIR $needs_restart %commandline);
 
-$PROGNAME = "check_sap_health";
-$REVISION = '$Revision: #PACKAGE_VERSION# $';
-$CONTACT = 'gerhard.lausser@consol.de';
-$TIMEOUT = 60;
-$STATEFILESDIR = '/var/tmp/check_sap_health';
 
-use constant OK         => 0;
-use constant WARNING    => 1;
-use constant CRITICAL   => 2;
-use constant UNKNOWN    => 3;
-use constant DEPENDENT  => 4;
-
-my @modes = (
-  ['server::connectiontime',
-      'connection-time', undef,
-      'Time to connect to the server' ],
-  ['server::connectiontime::sapinfo',
-      'sapinfo', undef,
-      'Time to connect and show system atttributes like sapinfo' ],
-  ['server::ccms::moniset::list',
-      'list-ccms-monitor-sets', undef,
-      'List all available monitor sets' ],
-  ['server::ccms::monitor::list',
-      'list-ccms-monitors', undef,
-      'List all monitors (can be restricted to a monitor set with --name)' ],
-  ['server::ccms::mte::list',
-      'list-ccms-mtes', undef,
-      'List all MTEs (must be restricted to a monitor set / monitor with --name/--name2)' ],
-  ['server::ccms::mte::check',
-      'ccms-mte-check', undef,
-      'Check all MTEs (must be restricted to a monitor set / monitor with --name/--name2)' ],
-);
-my $modestring = "";
-my $longest = length ((reverse sort {length $a <=> length $b} map { $_->[1] } @modes)[0]);
-my $format = "       %-".
-  (length ((reverse sort {length $a <=> length $b} map { $_->[1] } @modes)[0])).
-  "s\t(%s)\n";
-foreach (@modes) {
-  $modestring .= sprintf $format, $_->[1], $_->[3];
-}
-$modestring .= sprintf "\n";
-
-my $plugin = Nagios::MiniPlugin->new(
+my $plugin = Classes::Device->new(
     shortname => '',
     usage => 'Usage: %s [ -v|--verbose ] [ -t <timeout> ] '.
         '--mode <what-to-do> '.
         '--ashost <hostname> --sysnr <system number> '.
         '  ...]',
-    version => $REVISION,
+    version => '$Revision: #PACKAGE_VERSION# $',
     blurb => 'This plugin checks sap netweaver ',
     url => 'http://labs.consol.de/nagios/check_sap_health',
     timeout => 60,
-    shortname => '',
+    plugin => basename($0),
+);
+$plugin->add_mode(
+    internal => 'device::uptime',
+    spec => 'uptime',
+    alias => undef,
+    help => 'Check the uptime of the device',
+);
+
+$plugin->add_mode(
+    internal => 'server::connectiontime',
+    spec => 'connection-time',
+    alias => undef,
+    help => 'Time to connect to the server',
+);
+$plugin->add_mode(
+    internal => 'server::connectiontime::sapinfo',
+    spec => 'sapinfo',
+    alias => undef,
+    help => 'Time to connect and show system atttributes like sapinfo',
+);
+$plugin->add_mode(
+    internal => 'server::ccms::moniset::list',
+    spec => 'list-ccms-monitor-sets',
+    alias => undef,
+    help => 'List all available monitor sets',
+);
+$plugin->add_mode(
+    internal => 'server::ccms::monitor::list',
+    spec => 'list-ccms-monitors',
+    alias => undef,
+    help => 'List all monitors (can be restricted to a monitor set with --name)',
+);
+$plugin->add_mode(
+    internal => 'server::ccms::mte::list',
+    spec => 'list-ccms-mtes',
+    alias => undef,
+    help => 'List all MTEs (must be restricted to a monitor set / monitor with --name/--name2)',
+);
+$plugin->add_mode(
+    internal => 'server::ccms::mte::check',
+    spec => 'ccms-mte-check',
+    alias => undef,
+    help => 'Check all MTEs (must be restricted to a monitor set / monitor with --name/--name2)',
 );
 $plugin->add_arg(
     spec => 'ashost|H=s',
@@ -100,8 +101,7 @@ $plugin->add_arg(
 $plugin->add_arg(
     spec => 'mode=s',
     help => "--mode
-   A keyword which tells the plugin what to do
-$modestring",
+   A keyword which tells the plugin what to do",
     required => 1,
 );
 $plugin->add_arg(
@@ -147,6 +147,12 @@ $plugin->add_arg(
     required => 0,
 );
 $plugin->add_arg(
+    spec => 'negate=s%',
+    help => "--negate
+   The parameter allows you to map exit levels, such as warning=critical",
+    required => 0,
+);
+$plugin->add_arg(
     spec => 'with-mymodules-dyn-dir=s',
     help => '--with-mymodules-dyn-dir
    A directory where own extensions can be found',
@@ -165,92 +171,28 @@ $plugin->add_arg(
     required => 0,
 );
 
-
 $plugin->getopts();
-if ($plugin->opts->multiline) {
-  $ENV{NRPE_MULTILINESUPPORT} = 1;
-} else {
-  $ENV{NRPE_MULTILINESUPPORT} = 0;
-}
-if (! $plugin->opts->statefilesdir) {
-  if (exists $ENV{OMD_ROOT}) {
-    $plugin->override_opt('statefilesdir', $ENV{OMD_ROOT}."/var/tmp/check_sap_health");
-  } else {
-    $plugin->override_opt('statefilesdir', $STATEFILESDIR);
-  }
-}
-if (exists $plugin->opts->{opts}->{'with-mymodules-dyn-dir'}) {
-  $SAP::Server::my_modules_dyn_dir = $plugin->opts->{opts}->{'with-mymodules-dyn-dir'};
-} else {
-  $SAP::Server::my_modules_dyn_dir = '#MYMODULES_DYN_DIR#';
-}
+$plugin->classify();
+$plugin->validate_args();
 
 
-$plugin->{messages}->{unknown} = []; # wg. add_message(UNKNOWN,...)
-
-$plugin->{info} = []; # gefrickel
-
-if ($plugin->opts->mode =~ /^my-([^\-.]+)/) {
-  my $param = $plugin->opts->mode;
-  $param =~ s/\-/::/g;
-  push(@modes, [$param, $plugin->opts->mode, undef, 'my extension']);
-} elsif ($plugin->opts->mode eq 'encode') {
-  my $input = <>;
-  chomp $input;
-  $input =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-  printf "%s\n", $input;
-  exit 0;
-} elsif ((! grep { $plugin->opts->mode eq $_ } map { $_->[1] } @modes) &&
-    (! grep { $plugin->opts->mode eq $_ } map { defined $_->[2] ? @{$_->[2]} : () } @modes)) {
-  printf "UNKNOWN - mode %s\n", $plugin->opts->mode;
-  $plugin->opts->print_help();
-  exit 3;
-}
-if ($plugin->opts->name && $plugin->opts->name =~ /(%22)|(%27)/) {
-  my $name = $plugin->opts->name;
-  $name =~ s/\%([A-Fa-f0-9]{2})/pack('C', hex($1))/seg;
-  $plugin->override_opt('name', $name);
-}
-
-$SIG{'ALRM'} = sub {
-  printf "UNKNOWN - check_sap_health timed out after %d seconds\n",
-      $plugin->opts->timeout;
-  exit $ERRORS{UNKNOWN};
-};
-alarm($plugin->opts->timeout);
-
-$SAP::Server::plugin = $plugin;
-$SAP::Server::mode = (
-    map { $_->[0] }
-    grep {
-       ($plugin->opts->mode eq $_->[1]) ||
-       ( defined $_->[2] && grep { $plugin->opts->mode eq $_ } @{$_->[2]})
-    } @modes
-)[0];
-
-my $server = SAP::Server->new( runtime => {
-    plugin => $plugin,
-    options => {
-        verbose => $plugin->opts->verbose,
-    },
-},);
-#$server->dumper();
 if (! $plugin->check_messages()) {
-  $server->init();
+  $plugin->init();
   if (! $plugin->check_messages()) {
-    $plugin->add_message(OK, $server->get_summary())
-        if $server->get_summary();
-    $plugin->add_message(OK, $server->get_extendedinfo())
-        if $server->get_extendedinfo();
+    $plugin->add_ok($plugin->get_summary())
+        if $plugin->get_summary();
+    $plugin->add_ok($plugin->get_extendedinfo(" "))
+        if $plugin->get_extendedinfo();
   }
 } else {
-  $plugin->add_message(CRITICAL, 'wrong device');
+  $plugin->add_critical('wrong device');
 }
 my ($code, $message) = $plugin->opts->multiline ?
     $plugin->check_messages(join => "\n", join_all => ', ') :
     $plugin->check_messages(join => ', ', join_all => ', ');
-$message .= sprintf "\n%s\n", join("\n", @{$SAP::Server::info})
+$message .= sprintf "\n%s\n", $plugin->get_info("\n")
     if $plugin->opts->verbose >= 1;
-#printf "%s\n", Data::Dumper::Dumper($plugin->{info});
+#printf "%s\n", Data::Dumper::Dumper($plugin);
 $plugin->nagios_exit($code, $message);
+
 
