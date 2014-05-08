b21e788e5d851abe61a962efacc3f4133605b4f1
Gerhard Lausser <gerhard.lausser@consol.de>
2014-05-08 20:20:02 +0200

    add missing classes

---
+++ plugins-scripts/Classes/SAP.pm
@@ -0,0 +1,174 @@
+package Classes::SAP;
+our @ISA = qw(Classes::Device);
+
+use strict;
+use File::Basename;
+use Time::HiRes;
+use Digest::MD5 qw(md5_hex);
+use Data::Dumper;
+use AutoLoader;
+our $AUTOLOAD;
+
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+{
+  our $mode = undef;
+  our $plugin = undef;
+  our $blacklist = undef;
+  our $session = undef;
+  our $info = [];
+  our $extendedinfo = [];
+  our $summary = [];
+  our $oidtrace = [];
+  our $uptime = 0;
+}
+
+sub check_rfc_and_model {
+  my $self = shift;
+  chdir("/tmp");
+  if (eval "require sapnwrfc") {
+    my %params = (
+      'lcheck' => '1',
+    );
+    if ($self->opts->mode =~ /sapinfo/) {
+      $params{lcheck} = 0;
+    }
+    if ($self->opts->ashost) {
+      $params{ashost} = $self->opts->ashost;
+    }
+    if ($self->opts->sysnr) {
+      $params{sysnr} = $self->opts->sysnr;
+    }
+    if ($self->opts->client) {
+      $params{client} = $self->opts->client;
+    }
+    if ($self->opts->lang) {
+      $params{lang} = $self->opts->lang;
+    }
+    if ($self->opts->username) {
+      $params{user} = $self->opts->username;
+    }
+    if ($self->opts->password) {
+      $params{passwd} = $self->opts->password;
+    }
+    if ($self->opts->verbose) {
+      $params{debug} = '1';
+      $params{trace} = '1';
+    }
+    $self->{tic} = Time::HiRes::time();
+    my $session = undef;
+    eval {
+      $session = SAPNW::Rfc->rfc_connect(%params);
+    };
+    if ($@) {
+      $self->add_message(CRITICAL,
+          sprintf 'cannot create rfc connection: %s', $@);
+      $self->debug(Data::Dumper::Dumper(\%params));
+    } elsif (! defined $session) {
+      $self->add_message(CRITICAL,
+          sprintf 'cannot create rfc connection');
+      $self->debug(Data::Dumper::Dumper(\%params));
+    } else {
+      $Classes::SAP::session = $session;
+    }
+    $self->{tac} = Time::HiRes::time();
+  } else {
+    $self->add_message(CRITICAL,
+        'could not load perl module SAPNW');
+  }
+}
+
+sub session {
+  my $self = shift;
+  return $Classes::SAP::session;
+}
+
+sub init {
+  my $self = shift;
+  if ($self->mode =~ /^server::connectiontime/) {
+    my $fc = undef;
+    if ($self->mode =~ /^server::connectiontime::sapinfo/) {
+      eval {
+        my $fl = $self->session->function_lookup("RFC_SYSTEM_INFO");
+        $fc = $fl->create_function_call;
+        $fc->invoke();
+        printf "rrc %s\n", Data::Dumper::Dumper($fc->RFCSI_EXPORT);
+      };
+      if ($@) {
+        printf "crash %s\n", $@;
+      }
+      $self->{tac} = Time::HiRes::time();
+    }
+    $self->{connection_time} = $self->{tac} - $self->{tic};
+    $self->set_thresholds(warning => 1, critical => 5);
+    $self->add_message($self->check_thresholds($self->{connection_time}),
+         sprintf "%.2f seconds to connect as %s@%s",
+              $self->{connection_time}, $self->opts->username,
+              $self->session->connection_attributes->{sysId});
+    $self->add_perfdata(
+        label => 'connection_time',
+        value => $self->{connection_time},
+        warning => $self->{warning},
+        critical => $self->{critical},
+    );
+    if ($self->mode =~ /^server::connectiontime::sapinfo/) {
+      # extraoutput
+      # $fc
+    }
+  } elsif ($self->mode =~ /^server::ccms::/) {
+    $self->analyze_and_check_ccms_subsystem("Classes::SAP::Component::CCMS");
+  } elsif ($self->mode =~ /^my::([^:.]+)/) {
+    my $class = $1;
+    my $loaderror = undef;
+    substr($class, 0, 1) = uc substr($class, 0, 1);
+    if (! $self->opts->get("with-my-modules-dyn-dir")) {
+      $self->override_opt("with-my-modules-dyn-dir", "");
+    }
+    #foreach my $libpath (split(":", $self->opts->get("with-my-modules-dyn-dir"))) {
+    foreach my $libpath (split(":", "/omd/sites/lidl_monitoring/etc/check_sap_health")) {
+      foreach my $extmod (glob $libpath."/CheckSapHealth*.pm") {
+        eval {
+          $self->debug(sprintf "loading module %s", $extmod);
+          require $extmod;
+        };
+        if ($@) {
+          $loaderror = $extmod;
+          $self->debug(sprintf "failed loading module %s: %s", $extmod, $@);
+        }
+      }
+    }
+    my $obj = {
+        session => $self->session,
+        warning => $self->opts->warning,
+        critical => $self->opts->critical,
+    };
+    bless $obj, "My$class";
+    $self->{my} = $obj;
+    if ($self->{my}->isa("Classes::SAP")) {
+      my $dos_init = $self->can("init");
+      my $dos_nagios = $self->can("nagios");
+      my $my_init = $self->{my}->can("init");
+      my $my_nagios = $self->{my}->can("nagios");
+      if ($my_init == $dos_init) {
+          $self->add_message(UNKNOWN,
+              sprintf "Class %s needs an init() method", ref($self->{my}));
+      } else {
+        $self->{my}->init();
+      }
+    } else {
+      $self->add_message(UNKNOWN,
+          sprintf "Class %s is not a subclass of Classes::SAP%s",
+              ref($self->{my}),
+              $loaderror ? sprintf " (syntax error in %s?)", $loaderror : "" );
+    }
+  }
+}
+
+sub validate_args {
+  my $self = shift;
+  $self->SUPER::validate_args();
+  if ($self->opts->get("with-my-modules-dyn-dir")) {
+  }
+}
+
+
---
+++ plugins-scripts/Classes/SAP/Component/CCMS.pm
@@ -0,0 +1,566 @@
+package Classes::SAP::Component::CCMS;
+our @ISA = qw(GLPlugin::Item);
+
+use strict;
+use constant OK         => 0;
+use constant WARNING    => 1;
+use constant CRITICAL   => 2;
+use constant UNKNOWN    => 3;
+
+use constant MT_CLASS_NO_CLASS    => 0;
+use constant MT_CLASS_SUMMARY     => 50;
+use constant MT_CLASS_MONIOBJECT  => 70;
+use constant MT_CLASS_FIRST_MA    => 99;
+use constant MT_CLASS_PERFORMANCE => 100;
+use constant MT_CLASS_MSG_CONT    => 101;
+use constant MT_CLASS_SINGLE_MSG  => 102;
+use constant MT_CLASS_HEARTBEAT   => 103;
+use constant MT_CLASS_LONGTEXT    => 110;
+use constant MT_CLASS_SHORTTEXT   => 111;
+use constant MT_CLASS_VIRTUAL     => 199;
+# skip 50, 70, 110, 111, 199
+
+use constant AL_VAL_INAKTIV => 0;
+use constant AL_VAL_GREEN => 1;
+use constant AL_VAL_YELLOW => 2;
+use constant AL_VAL_RED => 3;
+
+
+sub session {
+  my $self = shift;
+  return $Classes::SAP::session;
+}
+
+sub init {
+  my $self = shift;
+  my($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) =
+      localtime(time);
+  my $bapi_tic = Time::HiRes::time();
+  if ($self->mode =~ /server::ccms::/) {
+    eval {
+      my $rcb = $self->session->function_lookup("SXMI_VERSIONS_GET");
+      my $tsl = $rcb->create_function_call;
+      #$tsl->INTERFACE("XMB"); # weglassen fuer alle interfaces
+      $tsl->invoke;
+      my $xmbversion = "0.1";
+      my $xalversion = "0.1";
+      foreach my $row (@{$tsl->VERSIONS}) {
+        $xmbversion = $row->{'VERSION'} if $row->{'INTERFACE'} eq "XMB";
+        $xalversion = $row->{'VERSION'} if $row->{'INTERFACE'} eq "XAL";
+      }
+      my $fl = $self->session->function_lookup("BAPI_XMI_LOGON");
+      my $fc = $fl->create_function_call;
+      $fc->EXTCOMPANY('LAUSSER');
+      $fc->EXTPRODUCT('CHECK_SAP_HEALTH');
+      $fc->INTERFACE('XAL');
+      $fc->parameter('VERSION')->value($xalversion);
+      $fc->invoke;
+      if ($fc->RETURN->{TYPE} !~ /^E/) {
+        if ($self->mode =~ /server::ccms::moniset::list/) {
+          $fl = $self->session->function_lookup("BAPI_SYSTEM_MS_GETLIST");
+          $fc = $fl->create_function_call;
+          $fc->EXTERNAL_USER_NAME("Agent");
+          $fc->invoke;
+          my @sets = @{$fc->MONITOR_SETS};
+          foreach (@sets) {
+            printf "%s\n", $_->{NAME};
+          }
+          $self->add_message(OK, "have fun");
+
+        } elsif ($self->mode =~ /server::ccms::monitor::list/) {
+          $fl = $self->session->function_lookup("BAPI_SYSTEM_MON_GETLIST");
+          # details with BAPI_SYSTEM_MS_GETDETAILS
+          $fc = $fl->create_function_call;
+          $fc->EXTERNAL_USER_NAME("Agent");
+          if ($self->opts->name) {
+            $fc->MONI_SET_NAME({
+                NAME => $self->opts->name,
+            });
+          }
+          $fc->invoke;
+          my @names = @{$fc->MONITOR_NAMES};
+          foreach my $ms (sort keys %{{ map {$_ => 1} map { $_->{MS_NAME} } @names }}) {
+            printf "%s\n", $ms;
+            foreach my $moni (sort keys %{{ map {$_ => 1} map { $_->{MONI_NAME} }
+                grep { $_->{MS_NAME} eq $ms } @names }}) {
+              printf " %s\n", $moni;
+            }
+          }
+          $self->add_message(OK, "have fun");
+        } elsif ($self->mode =~ /server::ccms::mte::/) {
+          if (! $self->opts->name || ! $self->opts->name2) {
+            die "__no_internals__you need to specify --name moniset --name2 monitor";
+          }
+          $fl = $self->session->function_lookup("BAPI_SYSTEM_MON_GETTREE");
+          $fc = $fl->create_function_call;
+          $fc->EXTERNAL_USER_NAME("Agent");
+          $fc->MONITOR_NAME({
+            MS_NAME => $self->opts->name,
+            MONI_NAME => $self->opts->name2,
+          });
+          $fc->invoke;
+          # TREE_NODES
+          if ($fc->RETURN->{TYPE} =~ /^E/) {
+            $self->add_message(CRITICAL, $fc->RETURN->{MESSAGE});
+          } else {
+            my @mtes = sort {
+                $a->{MTNAMELONG} cmp $b->{MTNAMELONG}
+            } grep {
+              $self->filter_name3($_->{MTNAMELONG})
+            } map { 
+                MTE->new(%{$_});
+            } @{$fc->TREE_NODES};
+            if ($self->mode =~ /server::ccms::mte::list/) {
+              foreach my $mte (@mtes) {
+                printf "%s %d\n", $mte->{MTNAMELONG}, $mte->{MTCLASS};
+              }
+            } elsif ($self->mode =~ /server::ccms::mte::check/) {
+              foreach my $mte (@mtes) {
+                next if ! grep $mte->{MTCLASS}, (100, 101);
+                #next if $mte->{ACTUAL_ALERT_DATA_VALUE} == 0;
+                $mte->collect_details($self->session);
+                if (keys %{$mte->perfdata()}) {
+                  $self->add_perfdata(%{$mte->perfdata()});
+                }
+                $self->add_message($mte->nagios_level(), $mte->nagios_message());
+              }
+              if (! @mtes) {
+                $self->add_message(UNKNOWN, "no mtes");
+              }
+            }
+          }
+        }
+        $fl = $self->session->function_lookup("BAPI_XMI_LOGOFF");
+        $fc = $fl->create_function_call;
+        $fc->INTERFACE('XAL');
+        $fc->invoke;
+      } else {
+        $self->add_message(CRITICAL, $fc->RETURN->{MESSAGE});
+      }
+    };
+    if ($@) {
+      my $message = $@;
+      $message =~ s/[^[:ascii:]]//g;
+      $message =~ s/\s+$//g;
+      chomp($message);
+      if ($message =~ /__no_internals__/) {
+        $message =~ s/at $0 line.*//g;
+        $message =~ s/__no_internals__//g;
+      }
+      $self->add_message(UNKNOWN, $message);
+    }
+  }
+  my $bapi_tac = Time::HiRes::time();
+  $self->set_thresholds(warning => 5, critical => 10);
+  #$self->add_message($self->check_thresholds($bapi_tac - $bapi_tic),
+  #     sprintf "runtime was %.2fs", $bapi_tac - $bapi_tic);
+  #$self->add_perfdata(
+  #    label => 'runtime',
+  #    value => $bapi_tac - $bapi_tic,
+  #    warning => $self->{warning},
+  #    critical => $self->{critical},
+  #);
+}
+
+sub map_alvalue {
+  my $self = shift;
+  my $value = shift;
+  if ($value && 1 <= $value && $value <= 3) {
+    return {
+      1 => OK,
+      2 => WARNING,
+      3 => CRITICAL,
+    }->{$value};
+  } else {
+    return UNKNOWN;
+  }
+}
+
+
+
+package MTE;
+our @ISA = qw(GLPlugin::TableItem);
+
+#our @ISA = qw(SAP::CCMS);
+# can't inherit, because this undefines the session handle. hurnmatz, greisliche
+
+use strict;
+
+use constant OK         => 0;
+use constant WARNING    => 1;
+use constant CRITICAL   => 2;
+use constant UNKNOWN    => 3;
+
+use constant { GREEN => 1, YELLOW => 2, RED => 3, GRAY => 4 };
+
+# http://www.benx.de/en/sap/program/RSALBAPI---code.htm
+use constant MT_CLASS_NO_CLASS    => 0;
+use constant MT_CLASS_SUMMARY     => 50;
+use constant MT_CLASS_MONIOBJECT  => 70;
+use constant MT_CLASS_FIRST_MA    => 99;
+use constant MT_CLASS_PERFORMANCE => 100;
+use constant MT_CLASS_MSG_CONT    => 101;
+use constant MT_CLASS_SINGLE_MSG  => 102;
+use constant MT_CLASS_HEARTBEAT   => 103;
+use constant MT_CLASS_LONGTEXT    => 110;
+use constant MT_CLASS_SHORTTEXT   => 111;
+use constant MT_CLASS_VIRTUAL     => 199;
+
+
+# Attribute Type
+#  Description
+#  
+# This graphic is explained in the accompanying text Performance Attribute
+#  Collects reported performance values and calculates the average
+#  
+# This graphic is explained in the accompanying text Status Attribute
+#  Reports error message texts and alert status
+#  
+# This graphic is explained in the accompanying text Heartbeat Attribute
+#  Checks whether components of the SAP system are active; if no values are reported for a monitoring attribute for a long time, it triggers an alert
+#  
+# This graphic is explained in the accompanying text Log Attribute
+#  Checks log and trace files (these attributes can use an existing log mechanism, such as the SAP system log, or they can be used by an application for the implementation of a separate log)
+#  
+# This graphic is explained in the accompanying text Text Attribute
+#  Allows a data supplier to report information that is not evaluated for alerts; the text can be updated as required
+#  
+
+
+our $separator = "\\";
+
+{
+  sub sap2nagios {
+    my $sap = shift;
+    my $nagios = 0;
+    if ($sap == 1) {
+      return OK;
+    } elsif ($sap == 2) {
+      return WARNING;
+    } elsif ($sap == 3) {
+      return CRITICAL;
+    } elsif ($sap == 4) {
+      return UNKNOWN;
+    } else {
+      return UNKNOWN;
+    }
+  }
+}
+
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = {};
+  bless $self, $class;
+  foreach (qw(MTSYSID MTCLASS MTMCNAME MTNUMRANGE MTUID MTINDEX EXTINDEX
+      OBJECTNAME MTNAMESHRT PRNTMTNAMESHRT)) {
+    $self->{$_} = $self->rstrip($params{$_}) if defined $params{$_};
+  }
+  foreach (qw(ALTREENUM ALIDXINTRE ALLEVINTRE ALPARINTRE VALINDEX)) {
+    $self->{$_} = $self->rstrip($params{$_}) if defined $params{$_};
+  }
+  # CUSGRPNAME = Eigenschaften: ..der MTE-Klasse
+  $self->{MTNAMELONG} = $self->mkMTNAMELONG;
+  $self->{TID} = $self->tid();
+  if ($self->{MTCLASS} == MT_CLASS_PERFORMANCE) {
+    bless $self, "MTE::Performance";
+  } elsif ($self->{MTCLASS} == MT_CLASS_MSG_CONT) {
+    bless $self, "MTE::ML";
+  } elsif ($self->{MTCLASS} == MT_CLASS_SINGLE_MSG) {
+    bless $self, "MTE::SM";
+  }
+  return $self;
+                my $bapi = {
+                    MT_CLASS_PERFORMANCE() => "BAPI_SYSTEM_MTE_GETPERFCURVAL",
+                    MT_CLASS_SINGLE_MSG() => "BAPI_SYSTEM_MTE_GETSMVALUE",
+                    #MT_CLASS_MSGCONT() => "BAPI_SYSTEM_MTE_?",
+                    MT_CLASS_LONGTEXT() => "BAPI_SYSTEM_MTE_GETMLCURVAL",
+                    MT_CLASS_SHORTTEXT() => "BAPI_SYSTEM_MTE_GETTXTPROP",
+                }->{$self->{MTCLASS}};
+
+}
+
+sub mkMTNAMELONG {
+  my $self = shift;
+  my $myname = "";
+  if ($self->{MTSYSID}) {
+    $myname = $myname.$self->{MTSYSID}.$MTE::separator;
+  } else {
+    #return undef;
+  }
+  $myname = $myname.$self->{MTMCNAME}.$MTE::separator;
+  if ($self->{PRNTMTNAMESHRT} && $self->{PRNTMTNAMESHRT} ne $self->{MTSYSID}.$MTE::separator.$self->{MTMCNAME}) {
+    $myname = $myname.$self->{PRNTMTNAMESHRT}.$MTE::separator;
+  }
+  if ($self->{OBJECTNAME}) {
+    $myname = $myname.$self->{OBJECTNAME}.$MTE::separator;
+  } else {
+    #return undef;
+  }
+  if ($self->{OBJECTNAME} ne $self->{MTNAMESHRT}) {
+    $myname = $myname.$self->{MTNAMESHRT};
+  }
+  return $myname;
+}
+
+sub collect_details {
+  my $self = shift;
+  my $session = shift;
+  my $fl = $session->function_lookup("BAPI_SYSTEM_MTE_GETGENPROP");
+  my $fc = $fl->create_function_call;
+  $fc->TID($self->tid);
+  $fc->EXTERNAL_USER_NAME("CHECK_SAP_HEALTH");
+  $fc->invoke;
+  #next if $fc->ACTUAL_ALERT_DATA->{VALUE} == 0;
+  $self->{ACTUAL_ALERT_DATA_VALUE} = $fc->ACTUAL_ALERT_DATA->{VALUE};
+  $self->{ACTUAL_ALERT_DATA_LEVEL} = $fc->ACTUAL_ALERT_DATA->{LEVEL};
+}
+
+sub perfdata {
+  my $self = shift;
+  return {};
+}
+
+sub nagios_level {
+  my $self = shift;
+  return MTE::sap2nagios($self->{ACTUAL_ALERT_DATA_VALUE});
+}
+
+sub nagios_message {
+  my $self = shift;
+  return "";
+}
+
+sub tid {
+  my $self = shift;
+  if (! exists $self->{tid}) {
+    $self->{tid} = {
+      MTSYSID => $self->{MTSYSID},
+      MTMCNAME => $self->{MTMCNAME},
+      MTNUMRANGE => $self->{MTNUMRANGE},
+      MTUID => $self->{MTUID},
+      MTCLASS => $self->{MTCLASS},
+      MTINDEX => $self->{MTINDEX},
+      EXTINDEX => $self->{EXTINDEX},
+    };
+  }
+  return $self->{tid};
+}
+
+sub extra_props {
+  my $self = shift;
+  my %params = @_;
+}
+
+sub rstrip {
+  my $self = shift;
+  my $message = shift;
+  $message =~ s/\s+$//g;
+  chomp $message;
+  return $message;
+}
+
+sub strip {
+  my $self = shift;
+  my $message = shift;
+  if (ref($message) eq "HASH") {
+    foreach (keys %{$message}) {
+      $self->strip($message->{$_});
+    }
+  } else {
+    $message =~ s/^\s+//g;
+    $message = $self->rstrip($message);
+  }
+  return $message;
+}
+
+
+package MTE::Performance;
+use strict;
+our @ISA = qw(MTE);
+use constant { GREEN => 1, YELLOW => 2, RED => 3, GRAY => 4 };
+
+sub collect_details {
+  my $self = shift;
+  my $session = shift;
+  $self->SUPER::collect_details($session);
+  my $fl = $session->function_lookup("BAPI_SYSTEM_MTE_GETPERFCURVAL");
+  my $fc = $fl->create_function_call;
+  $fc->TID($self->tid);
+  $fc->EXTERNAL_USER_NAME("CHECK_SAP_HEALTH");
+  $fc->invoke;
+  foreach (qw(ALRELEVVAL AVG15PVAL ALRELVALTI AVG05PVAL MAXPFDATE ALRELVALDT MINPFDATE
+      LASTPERVAL MAXPFTIME AVG15SVAL AVG15CVAL MAXPFVALUE AVG01PVAL AVG01SVAL MINPFVALUE
+      LASTALSTAT AVG01CVAL AVG05CVAL MINPFTIME AVG05SVAL)) {
+    $self->{$_} = $self->strip($fc->CURRENT_VALUE->{$_});
+  }
+  $fl = $session->function_lookup("BAPI_SYSTEM_MTE_GETPERFPROP");
+  $fc = $fl->create_function_call;
+  $fc->TID($self->tid);
+  $fc->EXTERNAL_USER_NAME("CHECK_SAP_HEALTH");
+  $fc->invoke;
+  foreach (qw(DECIMALS MSGID RELVALTYPE VALUNIT ATTRGROUP
+      TRESHR2Y THRESHDIR TRESHG2Y TRESHY2G THRESHSTAT MSGCLASS TRESHY2R)) {
+    $self->{$_} = $self->strip($fc->PROPERTIES->{$_});
+  }
+
+}
+
+sub perfdata {
+  my $self = shift;
+  my $perfdata = {
+    label => $self->{OBJECTNAME}."_".$self->{MTNAMESHRT},
+    value => $self->{ALRELEVVAL},
+  };
+  if ($self->{VALUNIT}) {
+    my $unit = lc $self->{VALUNIT};
+    $unit = "ms" if $unit eq "msec";
+    if ($unit =~ /^([u,m]{0,1}s|%|[kmgt]{0,1}b)$/) {
+      $perfdata->{uom} = $unit;
+    }
+  }
+  if ($self->{THRESHDIR} == 1 || $self->{THRESHDIR} == 2) {
+    if ($self->{THRESHDIR} == 1) {
+      #$self->set_thresholds(
+      #    warning => $self->{TRESHG2Y},
+      #    critical => $self->{TRESHY2R},
+      #);
+      $perfdata->{warning} = $self->{TRESHG2Y};
+      $perfdata->{critical} = $self->{TRESHY2R};
+    } else {
+      #$self->set_thresholds(
+      #    warning => $self->{TRESHG2Y}.":",
+      #    critical => $self->{TRESHY2R}.":",
+      #);
+      $perfdata->{warning} = $self->{TRESHG2Y}.":";
+      $perfdata->{critical} = $self->{TRESHY2R}.":";
+    }
+  }
+  return $perfdata;
+  #warning
+  #critical
+}
+
+sub nagios_message {
+  my $self = shift;
+  return return sprintf "%s %s = %s%s",
+      $self->{OBJECTNAME}, $self->{MTNAMESHRT},
+      $self->{ALRELEVVAL}, $self->{VALUNIT};
+}
+
+sub nagios_level {
+  my $self = shift;
+  if ($self->{ACTUAL_ALERT_DATA_VALUE} == 0) {
+    if ($self->{THRESHDIR} == 1 || $self->{THRESHDIR} == 2) {
+      # this mte is threshold driven
+      if ($self->{THRESHDIR} == 1) {
+        if ($self->{ALRELEVVAL} > $self->{TRESHY2R}) {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = RED;
+        } elsif ($self->{ALRELEVVAL} > $self->{TRESHG2Y}) {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = YELLOW;
+        } else {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = GREEN;
+        }
+      } else {
+        if ($self->{ALRELEVVAL} < $self->{TRESHY2R}) {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = RED;
+        } elsif ($self->{ALRELEVVAL} < $self->{TRESHG2Y}) {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = YELLOW;
+        } else {
+          $self->{ACTUAL_ALERT_DATA_VALUE} = GREEN;
+        }
+      }
+    }
+  }
+  return MTE::sap2nagios($self->{ACTUAL_ALERT_DATA_VALUE});
+}
+
+
+
+package MTE::ML;
+
+our @ISA = qw(MTE);
+
+sub collect_details {
+  my $self = shift;
+  my $session = shift;
+  $self->SUPER::collect_details($session);
+  my $fl = $session->function_lookup("BAPI_SYSTEM_MTE_GETMLCURVAL");
+  my $fc = $fl->create_function_call;
+  $fc->TID($self->tid);
+  $fc->EXTERNAL_USER_NAME("CHECK_SAP_HEALTH");
+  $fc->invoke;
+  foreach (qw(MSG)) {
+    $self->{$_} = $self->strip($fc->XMI_MSG_EXT->{$_});
+  }
+}
+
+sub nagios_message {
+  my $self = shift;
+  return $self->{MTNAMESHRT}." = ".$self->{MSG};
+}
+
+
+package MTE::SM;
+
+our @ISA = qw(MTE);
+
+sub collect_details {
+  my $self = shift;
+  my $session = shift;
+  $self->SUPER::collect_details($session);
+  my $fl = $session->function_lookup("BAPI_SYSTEM_MTE_GETSMVALUE");
+  my $fc = $fl->create_function_call;
+  $fc->TID($self->tid);
+  $fc->EXTERNAL_USER_NAME("CHECK_SAP_HEALTH");
+  $fc->invoke;
+  foreach (qw(MSG SMSGDATE SMSGDATE SMSGVALUE)) {
+    $self->{$_} = $self->strip($fc->VALUE->{$_});
+  }
+}
+
+sub nagios_message {
+  my $self = shift;
+  $self->{MSG} |= "<empty>";
+  return $self->{MTNAMESHRT}." = ".$self->{MSG};
+}
+
+
+__END__
+CALL FUNCTION 'BAPI_SYSTEM_MTE_GETGENPROP' "Read General Properties of a Monitor Tree Element
+  EXPORTING
+    tid =                       " bapitid       Monitor Tree Element ID
+    external_user_name =        " bapixmlogr-extuser  Name of the SAP-External User
+  IMPORTING
+    general_info =              " bapimtegen    General Data (Name, MTE Class and so on)
+    general_properties =        " bapimteprp    General Customizing data
+    general_values =            " bapimteval    Current General Values
+    last_value_time =           " bapialdate    Time of Last Value Change
+    highest_alert =             " bapiaid       ID of "Highest Alert"
+    highest_alert_data =        " bapialdata    Alert Value of "Highest Alert"
+    actual_alert =              " bapiaid       ID of "Actual Alert"
+    actual_alert_data =         " bapialdata    Alert Value of Actual Alert
+    collection_tool_def =       " bapitldef     Effectively Assigned Data Collection Method
+    onalert_tool_def =          " bapitldef     Effectively Assigned Auto-Reaction Method
+    analyze_tool_def =          " bapitldef     Effectively Assigned Analysis Method
+    collection_tool_run =       " bapitlrun     Runtime Information for Data Collection Method
+    onalert_tool_run =          " bapitlrun     Runtime Information for Auto-Reaction Method
+    parent_tid =                " bapitid       Parent Node ID
+    parent_data =               " bapiparent    Information About the Parent Node
+    return =                    " bapiret2      Return Messages
+    .  "  BAPI_SYSTEM_MTE_GETGENPROP
+
+actual_alert_data =
+VALUE ALVALUE INT4 000010  Alert value 
+SEVERITY ALSEVERITY INT4 000010  severity 
+
+VALUE = 
+AL_VAL_INAKTIV: White MTE, no data is being reported. Integer value 0.
+AL_VAL_GREEN: Green MTE, no alert is generated. Integer value 1.
+AL_VAL_YELLOW: Yellow MTE, yellow alert is generated. Integer value 2.
+AL_VAL_RED: Red MTE, highest possible alert, red alert is generated. Integer value 3.
+
+SEVERITY =
+Severity is measured with a scale from 0 (not important) to 255 (very important). 
+
